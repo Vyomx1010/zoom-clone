53ed24aed1f02b160664faed0a03d7d306f60296
VyomX.World <vyomx1010@gmail.com>
2025-05-23 11:14:01 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD
-  ? "http://localhost:8000"
-  : "http://localhost:8000";
+  ? "https://zoom-clone-kappa-steel.vercel.app"
+  : "https://zoom-clone-kappa-steel.vercel.app";
 
 export default server;
